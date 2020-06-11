43189b94dd162da90af93516af76620b62ac981c
Michael Nava <M1cha3l.nava@gmail.com>
2020-06-10 23:57:01 -0400

    added some structure to html and some variables to the js

--- assets/script.js
+++ assets/script.js
@@ -12,4 +12,44 @@ THEN the game is over
 
 WHEN the game is over
 THEN I can save my initials and score */
-console.log("Java linked correctly")
+
+//Declare variables
+var instructionHeading = document.querySelector("#instructionHeading");
+var questionHeading = document.querySelector("#questionHeading");
+var questionPar = document.querySelector("#questionPar");
+var score = 0;
+var highScores = {
+  firstPlace: "MN",
+  secondPlace: "GG",
+};
+
+var questionArray = [
+  "Commonly used data types DO NOT include:",
+  "The condition in an if / else statement is enclosed within ____.",
+  "Arrays in JavaScript can be used to store ____.",
+  "String values must be enclosed within ____ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",
+];
+
+var answerArray = [
+  "1. strings",
+  "2. booleans",
+  "3. alerts",
+  "4. numbers",
+  "1. quotes",
+  "2. curly brackets",
+  "3. parentheses",
+  "4. square brackets",
+  "1. numbers and strings",
+  "2. other arrays",
+  "3. booleans",
+  "4. all of the above",
+  "1. commas",
+  "2. curly brackets",
+  "3. quotes",
+  "4. parentheses",
+  "1. Javascript",
+  "2. terminal / bash",
+  "3. for loops",
+  "4. console.log",
+];
